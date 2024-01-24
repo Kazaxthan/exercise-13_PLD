d72ae25b864c6794075f16bd2305ae771d374bd1
Kazaxthan <carlo.paule.29@gmail.com>
2024-01-25 03:48:38 +0800

    Second commit, Here I finished coding where it will create the multiplication table from 1 - 10.

--- exercise13.py
+++ exercise13.py
@@ -1,2 +1,13 @@
 #   Exercise 13: Print multiplication table from 1 to 10
 
+for x in range(1, 11):
+    for y in range(1, 11):
+        
+        print(x * y, end="  ")
+    
+    print("\t\t")
+
+
+    
+    
+    
